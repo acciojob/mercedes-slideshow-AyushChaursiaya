400992fd80eebc2a86b012a83b4b8012166a4d56
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-10-21 13:31:14 +0000

    codE

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,34 @@
 import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
-const images = [
-  'https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem/image/MQ6-0-image-20220419101504/01-mercedes-benz-the-new-eqs-suv-x296-2021-1080x1350.jpeg',
-  'https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem_546277676/image/MQ6-0-image-20220819111550/02-mercedes-benz-the-new-eqs-suv-x296-2021-1680x1680.jpeg',
-  'https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem_354875624/image/MQ6-0-image-20220419101505/03-mercedes-benz-the-new-eqs-suv-x296-2021-1680x1680.jpeg'
-];
+// const images = [
+  
+// ];
 
 const App = () => {
-  const [currentImage, setCurrentImage] = useState(0);
+  const [images, setImages] = useState([
+    'https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem/image/MQ6-0-image-20220419101504/01-mercedes-benz-the-new-eqs-suv-x296-2021-1080x1350.jpeg',
+  'https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem_546277676/image/MQ6-0-image-20220819111550/02-mercedes-benz-the-new-eqs-suv-x296-2021-1680x1680.jpeg',
+  'https://www.mercedes-benz.com/en/vehicles/mercedes-eq/eqs-suv/_jcr_content/root/verticalgallery_copy/parsys/verticalgalleryitem_354875624/image/MQ6-0-image-20220419101505/03-mercedes-benz-the-new-eqs-suv-x296-2021-1680x1680.jpeg'
+  ]);
+
+  const [currentImage, setCurrentImage] = useState(images[0]);
+
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
-    }, 4000);
+  const timer = setInterval(() => {
+    setCurrentImage(oldImage => {
+      const newIndex = images.indexOf(oldImage) + 1;
+      return images[newIndex % images.length];
+    });
+  }, 5000);
 
-    return () => clearInterval(interval);
-  }, []);
+  return () => clearInterval(timer);  // This will clear the interval when the component unmounts.
+}, [images]);
 
-  return (
-    <div style={styles.slideshow}>
-      <img src={images[currentImage]} alt="Slideshow" style={styles.image} />
-    </div>
-  );
-}
-
-const styles = {
-  slideshow: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "500px",
-    width: "100%",
-    overflow: "hidden",
-  },
-  image: {
-    maxWidth: "100%",
-    maxHeight: "100%",
-    objectFit: "cover",
-  },
+return (
+  <img src={currentImage} alt="slideshow" />
+);
 };
 
 export default App;
